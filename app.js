fa2d1e509f981922cd76ab52d54c8cab7d6f4132
Robert Graupner <robert.graupnerr@gmail.com>
2022-05-18 16:30:17 +0200

    fix code formatting

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const maleNames = ['Robert', 'Daniel', 'Artur', 'Adam', 'Krystian', 'Mateusz', '
 const femaleNames = ['Magda', 'Dominika', 'Edyta', 'Dorota', 'Kasia', 'Gosia', 'Oliwia'];
 const lastNames = ['Nowak', 'Kowalski', 'Marchewka', 'Osadnik', 'Rolnik', 'Kutyba', 'Grab'];
 
-const randChoice = arr => {
+const randChoice = (arr) => {
 	const index = Math.floor(Math.random() * arr.length);
 	return arr[index];
 };
@@ -14,19 +14,19 @@ const people = [];
 
 for (let i = 0; i < 20; i++) {
 	const gender = randChoice(genders);
-	const firstName = (gender == "Female") ? randChoice(femaleNames) : randChoice(maleNames);
+	const firstName = gender == 'Female' ? randChoice(femaleNames) : randChoice(maleNames);
 	const lastName = randChoice(lastNames);
-  const age = Math.floor(Math.random() * (78 - 18)) + 18;
-  const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@gmail.com`;
-  const number = Math.floor(Math.random() * (999999999 - 500000000) + 500000000);
+	const age = Math.floor(Math.random() * (78 - 18)) + 18;
+	const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@gmail.com`;
+	const number = Math.floor(Math.random() * (999999999 - 500000000) + 500000000);
 
 	const person = {
 		gender,
 		firstName,
 		lastName,
-    age,
-    email,
-    number
+		age,
+		email,
+		number
 	};
 
 	people.push(person);
@@ -34,7 +34,7 @@ for (let i = 0; i < 20; i++) {
 
 const data = JSON.stringify(people);
 
-fs.writeFile("people.json", data, (err) => {
+fs.writeFile('people.json', data, (err) => {
 	if (err) throw err;
-	console.log("The file has been saved!");
+	console.log('The file has been saved!');
 });
